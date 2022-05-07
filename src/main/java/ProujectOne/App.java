ebcd14d3ee50737ace3707e6de1886e0f3d68d4d
JoshuaJimosn <jimsonjoshua379@gmail.com>
2022-05-07 20:19:17 +0530

    my first commit

---
+++ src/main/java/ProujectOne/App.java
@@ -0,0 +1,13 @@
+package ProujectOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ProujectOne/classOne.java
@@ -0,0 +1,11 @@
+package ProujectOne;
+
+public class classOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First project");
+
+	}
+
+}
