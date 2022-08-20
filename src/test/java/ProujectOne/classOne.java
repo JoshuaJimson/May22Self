dee7fc9da990988bb0ed2fd3be4f85e6212443b4
JoshuaJimosn <jimsonjoshua973@gmail.com>
2022-08-20 17:46:28 +0530

    Second Commit

--- src/test/java/ProujectOne/classOne.java
+++ src/test/java/ProujectOne/classOne.java
@@ -3,11 +3,13 @@ package ProujectOne;
 public class classOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("First project");
+		System.out.println("Joshua");
 		System.out.println("Testing Git");
 		System.out.println("Testing Git Pull");
-
+		int a = 10;
+		int b = 5;
+		int c = a + b;
+		System.out.println(c);
 	}
 
 }
